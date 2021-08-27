860070b9a2172bccd389b350c028c99c93e32ff8
Hoang Kiem <kiemhd@outlook.com>
2021-08-27 22:48:31 +0700

    Add auto get current public ip to git-deploy.sh file

--- git-deploy.sh
+++ git-deploy.sh
@@ -7,19 +7,14 @@ then
 fi
 
 PROJECT_NAME=$1
-
 USER_DIR='/home/git'
-
 GIT_DIR="${USER_DIR}/${PROJECT_NAME}"
+CURENT_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 git init --bare $GIT_DIR
-
 cp -r $USER_DIR/post-receive $GIT_DIR/hooks/ # Create a post-receive file in /home/git
-
 vi $GIT_DIR/hooks/post-receive
-
 chown -R git:git $GIT_DIR
 
 echo "You can add this repo to deploy"
-
-echo "git remote add live git@45.76.45.4:${PROJECT_NAME}"
+echo "git remote add live git@${CURENT_IP}:${PROJECT_NAME}"
