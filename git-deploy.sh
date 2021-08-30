69737a3ee012fba1aaef69890ccbb0f5fa63d1b0
Hoang Kiem <kiemhd@outlook.com>
2021-08-30 22:41:58 +0700

    Update auto replace domain with project name

--- git-deploy.sh
+++ git-deploy.sh
@@ -2,22 +2,32 @@
 
 if [ "$#" -ne 1 ]
 then
-  echo "Usage: git-deploy {project-name}"
+  echo "Usage: git-deploy {git-name}"
   exit 1
 fi
+# Take the search string
+read -p "Enter the theme name: " PROJECT_NAME
 
-PROJECT_NAME=$1
+GIT_NAME=$1
 USER_DIR='/home/git'
-GIT_DIR="${USER_DIR}/${PROJECT_NAME}"
-CURENT_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-# POST_RECEIVE_CONTENT=$(curl -L raw.githubusercontent.com/binjuhor/ee-git-deploy/master/post-receive)
+GIT_DIR="${USER_DIR}/${GIT_NAME}"
+SOURCE_DIR="/opt/easyengine/sites/${GIT_NAME}/app/htdocs/wp-content/themes/${PROJECT_NAME}"
+CURRENT_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 git init --bare $GIT_DIR
-# echo $POST_RECEIVE_CONTENT > $GIT_DIR/hooks/post-receive
 wget -qO post-receive raw.githubusercontent.com/binjuhor/ee-git-deploy/master/post-receive
+
+if [ $PROJECT_NAME != "" ]; then
+  sed -i.bak "s/\<theme_name\>/${PROJECT_NAME}/g" post-receive
+fi
+
+sed -i.bak "s/\<theme_name\>/${GIT_NAME}/g" post-receive
 mv post-receive $GIT_DIR/hooks/post-receive
 chmod +x $GIT_DIR/hooks/post-receive
-vi $GIT_DIR/hooks/post-receive # Let you edit your domain and config text
-chown -R git:git $GIT_DIR
+# vi $GIT_DIR/hooks/post-receive # Let you edit your domain and config text
+mkdir $SOURCE_DIR
 
-echo "You can add this repo to deploy"
-echo "git remote add live git@${CURENT_IP}:${PROJECT_NAME}"
+chown -R git:git $GIT_DIR
+chown -R www-data:www-data $SOURCE_DIR
+
+echo "You can add this repo to deploy your theme"
+echo "git remote add live git@${CURRENT_IP}:${GIT_NAME}"
