b6c92a9cbe95a23badbbb90dc26e60279d036f70
Hoang Kiem <kiemhd@outlook.com>
2021-09-02 11:18:28 +0700

    Removed the code color in post-received.sh file

--- post-receive.sh
+++ post-receive.sh
@@ -19,15 +19,15 @@ do
         sudo git --work-tree=$DEPLOY_DIR checkout -f $BRANCH
         sudo ee site clean $DOMAIN
         sudo chown -R www-data:www-data $DEPLOY_DIR
-        echo "You've just deployed your \e[1;92m${BRANCH}\033[0m branch to \e[1;92m${WEB_URL}\033[0m"
-        echo "Open \e[1;92m${WEB_URL}\033[0m to check your changed"
+        echo "You've just deployed your ${BRANCH} branch to ${WEB_URL}"
+        echo "Open ${WEB_URL} to check your changed"
    fi
 
    if [ "dev" == "$BRANCH" ];	then
         sudo git --work-tree=$DEV_DIR checkout -f $BRANCH
         sudo chown -R www-data:www-data $DEV_DIR
-        echo "You've just deployed your \e[1;92m${BRANCH}\033[0m branch to \e[1;92m${DEV_DOMAIN}\033[0m"
-        echo "Open \e[1;92m${DEV_URL}\033[0m to check your changed"
+        echo "You've just deployed your ${BRANCH} branch to ${DEV_DOMAIN}"
+        echo "Open ${DEV_URL} to check your changed"
    fi
 
    sudo chown -R git:git /home/git/$DOMAIN
